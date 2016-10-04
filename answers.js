31e34cd7ae26650650ab93e982f28757748b68c4
Yaron Cohen <yaroncnk@gmail.com>
2016-10-04 18:01:15 +0000

    submission 2

--- answers.js
+++ answers.js
@@ -5,4 +5,8 @@ function firstLetter(word) {
     return word.substring(0,1);
 }
 
-
+//Second function - find a string's second character
+function lastLetter(word) {
+    var howLong = word.length;
+    return word.substring(howLong-1,howLong);
+}
