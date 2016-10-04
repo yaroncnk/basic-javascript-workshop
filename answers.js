68872a593bf584293240c39c463f5193889955f6
Yaron Cohen <yaroncnk@gmail.com>
2016-10-04 18:25:24 +0000

    submission 5

--- answers.js
+++ answers.js
@@ -10,3 +10,18 @@ function lastLetter(word) {
     var howLong = word.length;
     return word.substring(howLong-1,howLong);
 }
+
+//Third function - input: a string and index ,  output - character in the location (index)
+function findChar(word,index){
+    return word.charAt(index);
+}
+
+//Fourth function - adding two numbers (if we add either num+string or string+string Javstring will merge them) 
+function addition(num1, num2) {
+    return num1 + num2;
+}
+
+//Fifth function - multiplyig two numbers. Input - two numbers , Output - x times y (unless not a number, then NaN)
+function multiplication(num1,num2) {
+    return num1 * num2;
+}
